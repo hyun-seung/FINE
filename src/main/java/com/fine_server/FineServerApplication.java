6e0edd57daf4f4584b0a232dd469de941fc12b84
임현승 <ardimento22@naver.com>
2022-05-25 15:26:48 +0900

    main

---
+++ src/main/java/com/fine_server/FineServerApplication.java
@@ -0,0 +1,13 @@
+package com.fine_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FineServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FineServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fine_server/FineServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fine_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FineServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
