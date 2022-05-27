5c4d152083519fea569291cffeb5a4ef5876029a
jungdahae1225 <jungdahae1225@gmail.com>
2022-05-27 16:11:33 +0900

    [ADD]Member exception resource

---
+++ src/main/java/com/fine_server/controller/mypage/errors/ErrorResult.java
@@ -0,0 +1,15 @@
+package com.fine_server.controller.mypage.errors;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+/**
+ * written by dahae
+ * date: 22.05.27
+ */
+
+@Data
+@AllArgsConstructor
+public class ErrorResult {
+    private String code;
+    private String message;
+}
