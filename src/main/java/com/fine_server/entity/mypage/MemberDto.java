269cb6912e38109de03a8f844b5fb85a87af2bb3
jungdahae1225 <jungdahae1225@gmail.com>
2022-06-23 16:25:40 +0900

    [add] Member intro

--- src/main/java/com/fine_server/entity/mypage/MemberDto.java
+++ src/main/java/com/fine_server/entity/mypage/MemberDto.java
@@ -4,14 +4,13 @@ import com.fine_server.entity.Member;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import javax.persistence.Column;
 import javax.validation.constraints.NotBlank;
 
 
 /**
  * written by dahae
- * date: 22.05.27
- * 사용자 이미지처리는 보류
+ * date: 22.06.23
+ * 사용자 이미지 처리와 키워드 처리는 다음 스프린트
  */
 
 @Data
@@ -21,10 +20,12 @@ public class MemberDto {
 
     @NotBlank(message = "닉네임 값은 필수 입력 값입니다.")
     private String nickname;
+    private String intro;
 
     public Member toEntity(){
         return Member.builder()
                 .nickname(nickname)
+                .intro(intro)
                 .build();
     }
 }
