d69f4f13e089be587401c185547906ab882919c6
kimehhk <kimehhk@gmail.com>
2022-07-06 02:05:07 +0900

    [SET] 참여하기 api url

--- src/main/java/com/fine_server/controller/Posting/PostingController.java
+++ src/main/java/com/fine_server/controller/Posting/PostingController.java
@@ -89,13 +89,4 @@ public class PostingController {
         return postingService.deleteRecruiting(recruitingId);
     }
 
-
-    // 신청 수락 (인원 체크 및 마감 여부 변경 포함)
-    @PostMapping("/{postingId}/{recruitingId}/accept")
-    public ResponseEntity<Recruiting> joinAccept(@RequestBody RecruitingDto recruitingDto, @PathVariable Long postingId, @PathVariable Long recruitingId) {
-        Recruiting recruiting = postingService.joinAccept(recruitingId, postingId, recruitingDto);
-        return new ResponseEntity(recruiting, HttpStatus.OK);
-    }
-
-
 }
