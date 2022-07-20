251d9db5e47461bb236567d671a3fc85feb3c8a7
정다해 <jdahae1225@naver.com>
2022-07-20 15:14:38 +0900

    fixed

---
+++ src/main/java/com/fine_server/controller/signup/phone/PhoneNum.java
@@ -0,0 +1,18 @@
+package com.fine_server.controller.signup.phone;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PhoneNum {
+    private String phoneNum;
+
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = "01063001337";
+    }
+}
