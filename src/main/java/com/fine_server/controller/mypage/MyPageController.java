1c6a3ffa7a023fa67d6607c53c7b7c9b0a3da489
kimehhk <kimehhk@gmail.com>
2022-08-12 20:20:41 +0900

    merge tmp

--- src/main/java/com/fine_server/controller/mypage/MyPageController.java
+++ src/main/java/com/fine_server/controller/mypage/MyPageController.java
@@ -80,12 +80,10 @@ public class MyPageController {
             throw new UserException("입력값이 잘못 되었습니다.");
         }
         Member member = memberService.editProfile(memberId,memberRequestDto);
-        List<String> keywordList = keywordService.save(member, memberRequestDto.getKeyword());//키워드 저장
 
-        MemberResponseDto memberResponseDto = new MemberResponseDto(member.getNickname(),member.getUserImageNum(),member.getIntro(),keywordList,0); //followBack 임시
-        return new ResponseEntity(memberResponseDto,HttpStatus.OK);
+        //MemberResponseDto memberResponseDto = new MemberResponseDto(member.getNickname(),member.getUserImageNum(),member.getIntro(),keywordList);
+        return new ResponseEntity(memberRequestDto,HttpStatus.OK);
     }
-
 
     /**
      * edit. 22.06.26
