3b5412dc14a54d16df101135ef3db17a51fecb25
정다해 <jdahae1225@naver.com>
2022-08-18 23:34:27 +0900

    [EDIT] Member Entity

--- src/main/java/com/fine_server/entity/Member.java
+++ src/main/java/com/fine_server/entity/Member.java
@@ -33,7 +33,7 @@ public class Member extends BaseEntity{
     @Column(name = "member_id")
     private Long id;
 
-    //@Column(nullable = false)
+    @Column(nullable = false)
     private String userId;
 
     @JsonIgnore
