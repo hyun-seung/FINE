3802efc3e79bc2c9bb711e86f186c3fee1f978e6
정다해 <jdahae1225@naver.com>
2022-09-11 14:47:12 +0900

    [ADD] UserNickName VaildCheck

---
+++ src/main/java/com/fine_server/controller/signup/dto/UserNickNameDto.java
@@ -0,0 +1,12 @@
+package com.fine_server.controller.signup.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class UserNickNameDto {
+    private String userNickName;
+}
