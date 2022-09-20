c00d7b35322e190c77d742190a29cad9c6f8056d
kimehhk <kimehhk@gmail.com>
2022-09-21 02:49:10 +0900

    [FIX] conflict resolve

--- src/main/java/com/fine_server/repository/MemberRepository.java
+++ src/main/java/com/fine_server/repository/MemberRepository.java
@@ -1,6 +1,7 @@
 package com.fine_server.repository;
 import com.fine_server.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
